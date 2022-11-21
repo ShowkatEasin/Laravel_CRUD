33f201dac3cba6f1a4f0c9c2a7da616af864d996
Showkat Easin <68397684+ShowkatEasin@users.noreply.github.com>
2022-11-21 17:11:13 +0600

    running

--- resources/views/raiyan.blade.php
+++ resources/views/raiyan.blade.php
@@ -1,5 +1,5 @@
 @include("backend.product.header")
-    <h1>Raiyan Bin Hossain</h1>
+    <h1>Showkat Easin</h1>
     <a href="{{route('abc')}}">welcome</a>
 
     @include("backend.product.footer")
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,6 @@
 </head>
 <body>
     <h1>Laravel</h1>
-    <a href="{{route('abm')}}">Raiyan</a>
+    <a href="{{route('abm')}}">Showkat</a>
 </body>
 </html>
